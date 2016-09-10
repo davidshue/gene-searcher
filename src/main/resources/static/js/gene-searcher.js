c0819faaba7286c4af7f755c08c9900ad6202cca
David Shue <david.shue@babycenter.com>
2016-09-09 20:59:09 -0700

    added twitter typeahead for the search ajax

--- src/main/resources/static/js/gene-searcher.js
+++ src/main/resources/static/js/gene-searcher.js
@@ -1,3 +1,22 @@
-$( document ).ready(function() {
+$(document).ready(function(){
+	$('input.main-search').typeahead(
+		{
+			hint: true,
+			highlight: true,
+			minLength: 2
+		},
+		{
+			source: function(query, processSync, processAsync) {
+				return $.ajax({
+					url: '/api/v1/suggest',
+					type: 'get',
+					data: {genes: query},
+					dataType: 'json',
+					success: function(json) {
+						return processAsync(json);
+					}
+				});
+			}
+		}
+	);
 });
-
